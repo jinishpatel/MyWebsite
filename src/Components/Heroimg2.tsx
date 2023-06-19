b3704df4996646fb6b0335cd185c025c83e11408
Jinish Patel <jinish5597@gmail.com>
2023-06-18 22:34:01 -0400

    hero2

--- src/Components/Heroimg2.tsx
+++ src/Components/Heroimg2.tsx
@@ -3,9 +3,9 @@ import IntoImg from "../assets/4102879_971.jpg";
 
 export const Heroimg2 = () => {
   return (
-    <div className="hero">
-      <div className="mask">
-        <img className="in-img" src={IntoImg} alt="Intro Image"></img>
+    <div className="hero2">
+      <div className="maskback">
+        <img className="in-img2" src={IntoImg} alt="Intro Image"></img>
       </div>
       <div className="heading">
         <h1>this is heading</h1>
