6ff2bb23a3eabb7d182b07151989a9e2036c0bb9
Jinish Patel <jinish5597@gmail.com>
2023-06-21 00:20:18 -0400

    skills1

--- src/Components/Main.tsx
+++ src/Components/Main.tsx
@@ -1,5 +1,4 @@
 import "./Main.css";
-import React from "react";
 
 export const Main = () => {
   return (
@@ -10,9 +9,32 @@ export const Main = () => {
       <div className="skills-container">
         <div className="Left">
           <h1>FrontEnd Technologies</h1>
+          <div className="technology-container">
+            <div className="tech-name">
+              <h4>HTML</h4>
+              <h4>CSS</h4>
+              <h4>JavaScript</h4>
+              <h4>React</h4>
+              <h4>TypeScript</h4>
+            </div>
+            <div className="tech-value">
+              <h4>asf</h4>
+            </div>
+          </div>
         </div>
         <div className="Right">
           <h1>BackEnd Technologies</h1>
+          <div className="technology-container">
+            <div className="tech-name">
+              <h4>Python</h4>
+              <h4>NodeJs</h4>
+              <h4>Java</h4>
+              <h4>C#</h4>
+            </div>
+            <div className="tech-value">
+              <h4>asf</h4>
+            </div>
+          </div>
         </div>
       </div>
     </div>
