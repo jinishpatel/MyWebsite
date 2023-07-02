beb92de34335cba744a3e1baec9d9b9b397f16ea
Jinish Patel <jinish5597@gmail.com>
2023-07-01 22:26:25 -0400

    error-solve

--- src/Components/Main.tsx
+++ src/Components/Main.tsx
@@ -50,13 +50,6 @@ export const Main = () => {
           <h1>BackEnd Technologies</h1>
           <div className="technology-container">
             <div className="tech-name">
-              <div>
-                <div className="chart" data-percent="90">
-                  <span>
-                    <strong>Java</strong>90%
-                  </span>
-                </div>
-              </div>
               <span>
                 <img src={htmlpng} alt="csspng" />
                 <h4>Python</h4>
